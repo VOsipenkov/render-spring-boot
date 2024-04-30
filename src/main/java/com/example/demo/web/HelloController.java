b9e61a9fd09ac002ddaafdab8ab437f5b5343bf1
VOsipenkov <JumpViaOcean@gmail.com>
2024-04-30 19:08:53 +0000

    add spring profile in Dockerfile

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController{
+    @Value("${application.message}")
+    private String message;
 
     @GetMapping("/hello")
     public String hello(){
